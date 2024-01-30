beac9ef7a3b7ee8ec389a652485293f45520fb58
unknown <amansoor964@gmail.com>
2024-01-30 13:20:24 +0300

    navbar hover over it

--- app/component/Navbar/Navbar.tsx
+++ app/component/Navbar/Navbar.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import style from "./navbar.module.css";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className={style.navbarWrapper}>
-      <div>Home</div>
-      <div>About</div>
+      <Link href={"/"} className={style.nabarselected}>
+        HOME
+      </Link>
+      <Link href={"https://www.linkedin.com/in/mansooralij/"}>ABOUTME</Link>
     </div>
   );
 };
